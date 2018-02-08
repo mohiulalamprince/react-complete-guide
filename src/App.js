4cbe7a299d9d18fa04deeecf94049d4cc53bc2e4
mohiulalamprince <mohiulalamprince@yahoo.com>
2018-02-08 17:59:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'test', age: 28
+      }, {
+        name: 'prince', age:29
+      }, {
+        name: 'bushra', age:26
+      }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    console.log("Was clicked!");
+    this.setState({
+      persons: [
+        {
+          name: newName, age: 27
+        }, {
+          name: 'prince', age:9
+        }, {
+          name: 'bushra', age:6
+        }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm react apps</h1>
+        <p>This is really working</p>
+        <button onClick={this.switchNameHandler.bind(this, 'maximalian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'max!')} >My best friend</Person>
+        <Person 
+          name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default App;
