da276fcaf4f2c6c117d498805687f4e5c936cbe9
mohiulalamprince <mohiulalamprince@yahoo.com>
2018-02-08 18:22:42 +0600

    name changed handler implement

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ class App extends Component {
   }
 
   switchNameHandler = (newName) => {
-    console.log("Was clicked!");
     this.setState({
       persons: [
         {
@@ -32,17 +31,33 @@ class App extends Component {
     });
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {
+          name: 'Max', age: 27
+        }, {
+          name: event.target.value, age:9
+        }, {
+          name: 'bushra', age:16
+        }
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm react apps</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler.bind(this, 'maximalian')}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('maximalian')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person 
           name={this.state.persons[1].name} age={this.state.persons[1].age} 
-          click={this.switchNameHandler.bind(this, 'max!')} >My best friend</Person>
+          click={this.switchNameHandler.bind(this, 'max!')} 
+          changed={this.nameChangedHandler}
+          >My best friend</Person>
         <Person 
           name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react'
 const person = (props) => {
     return (
         <div>
-            <p>I'm a Person! and I am {props.name}, {props.age} years old!</p>
+            <p onClick={props.click}>I'm a Person! and I am {props.name}, {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} />
         </div>
     )
 }
